3fbe3f795aa6e484c6aa36c09b0820846bc0989a
Rebekah Taylor <rebekahtdesigns@gmail.com>
2022-10-16 15:39:29 -0500

    displayed store locations

---
+++ src/components/Locations.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react"
+import "./Locations.css"
+
+
+export const Locations = () => {
+    const [locations, setLocations] = useState([])
+
+    useEffect(() => {
+
+        fetch('http://localhost:8088/locations')
+            .then(res => res.json())
+            .then((data) => {
+                setLocations(data)
+            })
+    }, [])
+
+    return (<>
+        <h1>Store Locations</h1>
+        <div className="location-container">
+            {
+                locations.map((location) => {
+                    return (
+                        <div key={location.id} className={location.id}>{location.address}{location.sqFt}</div>
+                    )
+                })
+            }
+        </div>
+    </>)
+}
